98b2998dfb670b94b6baa9f01d0e2e2cbc69e64a
Eowyn Baughman <eowyn42@gmail.com>
2018-10-05 16:05:44 -0700

    Most recent django-related  post on stack overflow

---
+++ stackoverflower.py
@@ -0,0 +1,16 @@
+import requests
+
+url = 'https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&tagged=django&site=stackoverflow'
+response = requests.get(url)
+
+question = response.json()['items'][0]
+
+title = question['title']
+user_id = question['owner']['user_id']
+display_name = question['owner']['display_name']
+
+response = requests.get('https://api.stackexchange.com/2.2/users/{}?order=desc&sort=reputation&site=stackoverflow'.format(user_id))
+
+creation_date = response.json()['items'][0]['creation_date']
+
+print("{} created on {} asked \"{}\"".format(display_name, creation_date, title))
